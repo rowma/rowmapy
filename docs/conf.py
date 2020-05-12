d5f4d6beff366bf8564c4ba5124ae3b40877c793
asmsuechan <suenagaryoutaabc@gmail.com>
2020-05-12 17:53:44 +0900

    Fix sphinx builld in netlify

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'asmsuechan'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0.3'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
